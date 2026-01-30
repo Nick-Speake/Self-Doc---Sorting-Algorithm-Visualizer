184b23d0b6e7001e510c50b2a96ae5a11648678d
Nick-Speake <nicspeake@gmail.com>
2026-01-30 04:35:10 -0600

    Add files via upload

---
+++ sorting_algorithms/main.py
@@ -0,0 +1,7 @@
+from . import create_app
+
+app = create_app() # instantiates Flask object
+    
+if __name__ == "__main__":
+    app.run(debug=True) # debug=True enables live reloads for dynamic web application
+    
